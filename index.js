f9dc95392ff8289431eb157a4f3dc6d21058180d
Y-hatter <65300527+Y-hatter@users.noreply.github.com>
2025-07-25 21:32:38 +0530

    exposed functions to window

--- index.js
+++ index.js
@@ -7,38 +7,48 @@ let scoreHomeEl = document.getElementById("score-home")
 let scoreGuestEl = document.getElementById("score-guest")
 
 
-
-
 function plus1Home(){
     scoreHome +=1
     scoreHomeEl.textContent = scoreHome    
 }
+
+window.plus1Home = plus1Home
+
 
 function plus2Home(){
     scoreHome +=2
     scoreHomeEl.textContent = scoreHome    
 }
 
+window.plus2Home = plus2Home
+
 function plus3Home(){
     scoreHome +=3
     scoreHomeEl.textContent = scoreHome    
 }
 
+window.plus3Home = plus3Home 
 
 function plus1Guest(){
     scoreGuest +=1
     scoreGuestEl.textContent = scoreGuest    
 }
 
+window.plus1Guest = plus1Guest
+
 function plus2Guest(){
     scoreGuest +=2
     scoreGuestEl.textContent = scoreGuest  
 }
 
+window.plus2Guest = plus2Guest
+
 function plus3Guest(){
     scoreGuest +=3
     scoreGuestEl.textContent = scoreGuest   
 }
+
+window.plus3Guest = plus3Guest
 
 function newGame(){
     scoreHomeEl.textContent = 0
@@ -47,3 +57,4 @@ function newGame(){
     scoreGuest = 0
 }
 
+window.newGame = newGame
